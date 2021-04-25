1884dd8b0837a8e04e16a34ab5916235409cb264
Ryan Simkoff <rsimkoff@gmail.com>
2021-04-24 20:36:54 -0400

    updating repo

---
+++ app.js
@@ -0,0 +1,32 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+const employees = [];
+
+function initApp() {
+    startHtml();
+    addMember();
+}
+
+function addMember() {
+    inquirer.prompt([{
+        message: "Enter team member's name",
+        name: "name"
+    },
+    {
+        type: "list",
+        message: "Select team member's role",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Manager"
+        ],
+        name: "role"
+    },
+    {
+        message: "Enter team member's id",
+        name: "id"
+    },
